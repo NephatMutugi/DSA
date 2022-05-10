e15bf10e748cbf1a32aa053a0212d53769cee492
KEN20960 <muchirinephat5@gmail.com>
2022-05-10 11:43:42 +0300

    Demo list

---
+++ src/linked_lists/customer/Demo.java
@@ -0,0 +1,25 @@
+package linked_lists.customer;
+
+import java.util.LinkedList;
+
+public class Demo {
+    public static void main(String[] args) {
+        LinkedList<String > placesToVisit = new LinkedList<>();
+        placesToVisit.add("Castle Forest Lounge");
+        placesToVisit.add("Ragia Forest");
+        placesToVisit.add("Chalbi Desert");
+        placesToVisit.add("Ngong Hills");
+        placesToVisit.add("Mt. Suswa");
+        placesToVisit.add("Mt. Kilimambogo");
+        placesToVisit.add("Seven Ponds");
+        printList(placesToVisit);
+
+    }
+
+    private static void printList(LinkedList<String > linkedList){
+        for (String s : linkedList) {
+            System.out.println("Now visiting: " + s);
+        }
+        System.out.println("====================================");
+    }
+}

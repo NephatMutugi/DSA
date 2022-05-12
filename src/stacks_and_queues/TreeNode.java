d3ff2650ea94343c7f3ea901aa0b3622237edcc4
KEN20960 <muchirinephat5@gmail.com>
2022-05-12 13:10:28 +0300

    Binary Tree inorder transversal

---
+++ src/stacks_and_queues/TreeNode.java
@@ -0,0 +1,20 @@
+package stacks_and_queues;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

ddcf7e8b7fccb5f2fd41e74fca57d2c4bf7c03df
KEN20960 <muchirinephat5@gmail.com>
2022-05-16 09:02:12 +0300

    Lowest Common Ancestor

---
+++ src/data_structures/trees/LowestCommonAncestor.java
@@ -0,0 +1,8 @@
+package data_structures.trees;
+
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+}

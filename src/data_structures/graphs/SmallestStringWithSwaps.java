7d3a8d7dceca05207b3e6ea0539bc42a05fe9226
KEN20960 <muchirinephat5@gmail.com>
2022-05-16 10:35:18 +0300

    Graphs

---
+++ src/data_structures/graphs/SmallestStringWithSwaps.java
@@ -0,0 +1,10 @@
+package data_structures.graphs;
+
+import java.util.List;
+
+public class SmallestStringWithSwaps {
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+
+        return s;
+    }
+}

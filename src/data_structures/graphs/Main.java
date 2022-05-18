a7cb7f3ca5da3873bbe194b6da4fc05f97a6580d
KEN20960 <muchirinephat5@gmail.com>
2022-05-18 14:35:37 +0300

    Graphs -> Number of provinces

---
+++ src/data_structures/graphs/Main.java
@@ -0,0 +1,7 @@
+package data_structures.graphs;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/data_structures/graphs/NumberOfProvinces.java
+++ src/data_structures/graphs/NumberOfProvinces.java
@@ -2,7 +2,25 @@ package data_structures.graphs;
 
 public class NumberOfProvinces {
     public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int components = 0;
+        for(int vertex = 0; vertex < n; vertex++) {
+            if(!visited[vertex]) {
+                visited[vertex] = true;
+                components++;
+                visitNeighbors(vertex, isConnected, visited);
+            }
+        }
+        return components;
+    }
 
-        return 0;
+    private void visitNeighbors(int vertex, int[][] isConnected, boolean[] visited) {
+        for(int v = 0; v < isConnected.length; v++) {
+            if(isConnected[vertex][v] == 1 && !visited[v]) {
+                visited[v] = true;
+                visitNeighbors(v, isConnected, visited);
+            }
+        }
     }
 }

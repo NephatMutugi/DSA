a41f89cc2c0a935d6882f57d779d3072c240c46e
KEN20960 <muchirinephat5@gmail.com>
2022-05-19 11:05:29 +0300

    Arrays -> Valid Subsequence

--- src/data_structures/arrays/ValidSubsequence.java
+++ src/data_structures/arrays/ValidSubsequence.java
@@ -1,4 +1,21 @@
 package data_structures.arrays;
 
 public class ValidSubsequence {
+    public static void main(String[] args) {
+        int [] bigArr = {1, 2, 3, 4};
+        int [] smallArr = {1, 3, 4};
+
+        System.out.println(isValidSubSequence(bigArr, smallArr));
+    }
+    public static boolean isValidSubSequence(int [] bigArr, int [] subArr){
+        int i = 0, j = 0;
+        while (i < bigArr.length && j < subArr.length){
+            if (subArr[j] == bigArr[i]){
+                j++;
+            }
+            i++;
+        }
+
+        return j == subArr.length;
+    }
 }

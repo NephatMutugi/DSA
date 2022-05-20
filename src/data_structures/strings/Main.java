b9de3f5d6cc2d9274e8d76c7fa78da81046033ee
KEN20960 <muchirinephat5@gmail.com>
2022-05-20 13:09:57 +0300

    Strings -> Valid Palindrome

--- src/data_structures/strings/Main.java
+++ src/data_structures/strings/Main.java
@@ -5,15 +5,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        RestoreIPAddresses restoreIPAddresses = new RestoreIPAddresses();
+        // Restore IP Addresses
 
+        RestoreIPAddresses restoreIPAddresses = new RestoreIPAddresses();
         String ipAddress = "25525511135";
         List<?> myList = restoreIPAddresses.restoreIpAddresses(ipAddress);
-
         for (int i = 0; i < myList.size(); i++) {
             System.out.println("IP " + (i +1) + " -> " + myList.get(i));
         }
+        System.out.println("##################################\n");
 
-        System.out.println();
+        // Valid Palindrome
+        ValidPalindrome palindrome = new ValidPalindrome();
+        String palindromeTest = "A man, a plan, a canal: Panama";
+        System.out.println("Is palindrome -> " + palindrome.isPalindrome(palindromeTest));
     }
 }
--- src/data_structures/strings/ValidPalindrome.java
+++ src/data_structures/strings/ValidPalindrome.java
@@ -1,8 +1,31 @@
 package data_structures.strings;
 
+
 public class ValidPalindrome {
      public boolean isPalindrome (String s){
+         StringBuilder stringBuilder = new StringBuilder();
+         for (char c: s.toCharArray()){
+             if (Character.isDigit(c) || Character.isLetter(c)){
+                 stringBuilder.append(c);
+             }
+         }
 
-         return false;
+         stringBuilder = new StringBuilder(stringBuilder.toString().toLowerCase());
+         String fixedString = stringBuilder.toString();
+         System.out.println("String to test -> " + fixedString);
+
+         int pointer1 = 0;
+         int pointer2 = fixedString.length() - 1;
+
+         while (pointer1 <= pointer2){
+             // Traverses to the middle and runs n/2
+             if (fixedString.charAt(pointer1) != fixedString.charAt(pointer2)){
+                 return false;
+             }
+
+             pointer1 += 1;
+             pointer2 -= 1;
+         }
+         return true;
      }
 }

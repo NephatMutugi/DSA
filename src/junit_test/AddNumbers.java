73ae16c7b6d11d0acc4b97545cb2de959f808623
KEN20960 <muchirinephat5@gmail.com>
2022-05-25 15:39:01 +0300

    JUnit Test

--- src/junit_test/AddNumbers.java
+++ src/junit_test/AddNumbers.java
@@ -1,12 +1,12 @@
 package junit_test;
 
 public class AddNumbers {
-    public static void main(String[] args) {
-        int [] nums = {1,2,3,4,5};
-        int sum = 0;
+   int myCalc(int [] nums){
+       int sum = 0;
 
-        for (int a : nums)
-            sum += a;
-        System.out.println("SUM -> " + sum);
-    }
+       for (int a : nums)
+           sum += a;
+       System.out.println("SUM -> " + sum);
+       return sum;
+   }
 }
---
+++ tests/MyAddNumbersTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyAddNumbersTest {
+
+    @Test
+    public void test(){
+        fail("Not yet implemented");
+    }
+}
